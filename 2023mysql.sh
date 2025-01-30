20119cbb9c860991a4c5212fe2311f95e4037537
dhanu-sri01 <dhanusribattula@gmail.com>
2025-01-30 13:09:19 +0530

    Update 2023mysql.sh

--- 2023mysql.sh
+++ 2023mysql.sh
@@ -3,4 +3,3 @@ sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
 sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
 sudo dnf install mysql-community-client -y
 sudo dnf install mysql-community-server -y
-FHD
